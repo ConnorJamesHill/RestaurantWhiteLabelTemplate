d92885af2909e5b7571c4c9f5852a2f9eca1ced5
Meruem Moniker <jamesconnorhill@gmail.com>
2025-05-02 17:27:06 -0400

    Initial Commit

---
+++ Restaurant Template/Restaurant_TemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Restaurant_TemplateApp.swift
+//  Restaurant Template
+//
+//  Created by Connor Hill on 5/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Restaurant_TemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
